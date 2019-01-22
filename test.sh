eb7e09a9a847356d118659e45c9874758f3842ca
Jahed Ahmed <jahed.public@gmail.com>
2019-01-22 23:45:17 +0000

    test: ensure terraform executes and fails correctly

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+EXPECTED_VERSION=$(git describe | cut -d'-' -f1)
+
+echo "test: execute terraform"
+yarn install > /dev/null 2>&1
+RESULT=$(./bin/terraform.js --version)
+if [[ "${RESULT}" != "Terraform ${EXPECTED_VERSION}" ]]; then
+  echo "Test failed."
+  exit 1
+fi
+
+echo "test: forward exit code"
+set +e
+./bin/terraform.js --bad-arg > /dev/null 2>&1
+RESULT=$?
+set -e
+if [[ "${RESULT}" != "127" ]]; then
+  echo "Test failed."
+  exit 1
+fi
+
+echo "test: reinstall terraform"
+rimraf downloads > /dev/null 2>&1
+yarn install > /dev/null 2>&1
+RESULT=$(./bin/terraform.js --version)
+if [[ "${RESULT}" != "Terraform ${EXPECTED_VERSION}" ]]; then
+  echo "Test failed."
+  exit 1
+fi
+
+echo "Tests passed."
