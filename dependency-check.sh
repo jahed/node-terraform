a95ac82a4293b540eb33665b7db3eeba80395ff9
Jahed <jahed.public@gmail.com>
2019-10-11 18:08:01 +0100

    ci: run tests after commit to avoid running on no changes

--- dependency-check.sh
+++ dependency-check.sh
@@ -2,8 +2,19 @@
 set -eo pipefail
 
 yarn upgrade --latest
-yarn test
 git add yarn.lock '*package.json'
+
+set +e
 git commit -m 'build(deps): upgrade dependencies'
+RESULT=$?
+set -e
+
+if [[ "${RESULT}" != "0" ]]; then
+  echo "No dependency changes."
+  exit 0
+fi
+
+yarn test
+
 git remote set-url --push origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 git push
