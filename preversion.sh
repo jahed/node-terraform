5195e6240236c0b7e4cf121509ef97b9d9093d58
Jahed <jahed.public@gmail.com>
2020-08-21 13:42:09 +0100

    chore: correctly import gpg key

--- preversion.sh
+++ preversion.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+gpg --import hashicorp.asc || true
+
 VERSION=$(git describe | cut -d'-' -f1 | cut -d'v' -f2)
 curl -s "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_SHA256SUMS" > hashes.txt
 curl -s "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_SHA256SUMS.sig" \
-  | gpg --keyring hashicorp.asc --verify - hashes.txt
+  | gpg --verify - hashes.txt
 
 git add hashes.txt
 echo "Updated hashes.txt."
