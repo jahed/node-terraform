6bcb90354b71f450af883341dedd1d2bdc7eb74d
Jahed <jahed.public@gmail.com>
2020-08-28 13:49:21 +0100

    test: ignore version output complaining about old version

--- test.sh
+++ test.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+function get_version_line {
+  ./bin/terraform.js --version | fgrep 'Terraform v' | head -n1
+}
+
 EXPECTED_VERSION=$(git describe | cut -d'-' -f1)
 
-echo "test: execute terraform"
-yarn install --frozen-lockfile > /dev/null 2>&1
-RESULT=$(./bin/terraform.js --version | head -n1)
+echo "TEST: Executes existing Terraform"
+yarn install --frozen-lockfile
+RESULT=$(get_version_line)
 if [[ "${RESULT}" != "Terraform ${EXPECTED_VERSION}" ]]; then
   echo "Test failed."
   exit 1
 fi
+echo
 
-echo "test: forward exit code"
+echo "TEST: Forwards exit codes"
 set +e
 ./bin/terraform.js --bad-arg > /dev/null 2>&1
 RESULT=$?
@@ -20,14 +25,16 @@ if [[ "${RESULT}" != "127" ]]; then
   echo "Test failed."
   exit 1
 fi
+echo
 
-echo "test: reinstall terraform"
-rimraf downloads > /dev/null 2>&1
-yarn install --frozen-lockfile > /dev/null 2>&1
-RESULT=$(./bin/terraform.js --version | head -n1)
+echo "TEST: Reinstalls Terraform with correct version"
+rimraf downloads
+yarn install --frozen-lockfile
+RESULT=$(get_version_line)
 if [[ "${RESULT}" != "Terraform ${EXPECTED_VERSION}" ]]; then
   echo "Test failed."
   exit 1
 fi
+echo
 
 echo "Tests passed."
