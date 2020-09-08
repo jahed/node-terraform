828fdabacbe9ed2eaacb8ef2932ffc2282d0376e
Jahed <jahed.public@gmail.com>
2020-09-08 14:49:29 +0100

    refactor: mkdir recursive is stable so remove mkdirp

--- src/makeDirectory.js
+++ src/makeDirectory.js
@@ -1,8 +1,8 @@
-const mkdirp = require('mkdirp')
+const fs = require('fs')
 
 function makeDirectory(directory) {
   console.log(`making directory`, { directory })
-  return mkdirp(directory)
+  return fs.promises.mkdir(directory, { recursive: true })
 }
 
 module.exports = makeDirectory
