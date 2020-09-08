1d14b52a5bf7d07e40628f2c587ea97048dee4cf
Jahed <jahed.public@gmail.com>
2020-09-08 14:09:27 +0100

    chore: fail when curl request fails

--- version.sh
+++ version.sh
@@ -3,18 +3,25 @@
 # https://docs.npmjs.com/misc/scripts
 set -eo pipefail
 
-echo "Updating hashes.txt."
-
-gpg --import hashicorp.asc || true
-
 VERSION=$(cat package.json | jq -r '.version' | cut -d'-' -f1)
+
+echo "UPDATING HASHES"
 echo "Downloading hashes for ${VERSION}"
-curl -s "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_SHA256SUMS" > hashes.txt
-curl -s "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_SHA256SUMS.sig" \
+curl -s --fail --show-error "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_SHA256SUMS" > hashes.txt
+echo
+
+echo "VERIFYING HASHES"
+gpg --import hashicorp.asc || true
+curl -s --fail --show-error "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_SHA256SUMS.sig" \
   | gpg --verify - hashes.txt
+echo
 
+echo "RUNNING TESTS"
 yarn test
+echo
 
+echo "STAGING HASHES"
 git add hashes.txt
+echo
 
 echo "Done."
