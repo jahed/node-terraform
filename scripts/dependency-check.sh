c6249286b4994ef6c513f39bee69a754622ac80b
Jahed <jahed.public@gmail.com>
2022-10-28 12:38:50 +0100

    chore: support major upgrades

--- scripts/dependency-check.sh
+++ scripts/dependency-check.sh
@@ -6,27 +6,38 @@ if [[ "${CI}" == "true" ]]; then
   git config user.name "${GITHUB_ACTOR}"
 fi
 
-echo "UPGRADING DEPENDENCIES"
+echo "INSTALLING DEPENDENCIES"
 npm ci
+echo
+
+echo "UPGRADING MINOR DEPENDENCIES"
 npx ncu --target minor --upgrade
 npm install
+git add package-lock.json package.json
+if ! git diff --quiet --cached --exit-code; then
+  git commit -m 'build(deps): upgrade minor dependencies'
+fi
+echo
+
+echo "UPGRADING TRANSITIVE DEPENDENCIES"
 npm upgrade
 npm install
-echo
-
-echo "AUDITING DEPENDENCIES"
-npm audit
-echo
-
-echo "COMMITTING CHANGES"
 git add package-lock.json package.json
-set +e
-git commit -m 'build(deps): upgrade dependencies'
-RESULT=$?
-set -e
+if ! git diff --quiet --cached --exit-code; then
+  git commit -m 'build(deps): upgrade transitive dependencies'
+fi
 echo
 
-if [[ "${RESULT}" != "0" ]]; then
+if test "${INPUT_UPGRADELEVEL}" = "major"; then
+  echo "UPGRADING MAJOR DEPENDENCIES"
+  npx ncu --target major --upgrade
+else
+  echo "CHECKING MAJOR DEPENDENCIES"
+  npx ncu --target major
+fi
+echo
+
+if git diff --quiet --exit-code master origin/master; then
   cat <<EOF
 RESULT:
   No dependencies upgraded.
@@ -35,6 +46,10 @@ Done.
 EOF
   exit 0
 fi
+
+echo "AUDITING DEPENDENCIES"
+npm audit --audit-level=critical
+echo
 
 echo "LINTING"
 npm run lint
