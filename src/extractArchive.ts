98176d6a96d113a5cd7fb4c124092c4d6bb41be0
jahed.dev <3330963+jahed@users.noreply.github.com>
2025-09-19 22:11:15 +0000

    build(deps): replace yauzl with fflate

--- src/extractArchive.ts
+++ src/extractArchive.ts
@@ -1,77 +1,40 @@
-import { eventually, reason } from "@jahed/promises";
-import fs from "fs";
-import path from "path";
-import yauzl from "yauzl";
+import fs from "node:fs/promises";
+import path from "node:path";
+import fflate, { Unzipped } from "fflate";
 import { debug } from "./debug";
 import { ExtractArgs } from "./types";
 
-const extractArchive = ({ outputs, buffer, outdir }: ExtractArgs) => {
+export const extractArchive = async ({
+  outputs,
+  buffer,
+  outdir,
+}: ExtractArgs): Promise<string> => {
   debug("extracting", { outdir });
-  return eventually((resolve, reject) => {
-    yauzl.fromBuffer(buffer, { lazyEntries: true }, (err, archive) => {
-      if (err) {
-        reject(err);
-        return;
-      }
 
-      if (!archive) {
-        // bad @types/yauzl
-        reject(new Error("failed to read archive and no reason was given."));
-        return;
-      }
-
-      archive.on("error", (error) => reject(error));
-
-      archive.on("entry", (entry) => {
-        if (entry.fileName !== outputs.originalFilename) {
-          // ignore LICENSE.txt and any other additional files
-          archive.readEntry();
-          return;
+  const { [outputs.originalFilename]: fileContent } =
+    await new Promise<Unzipped>((resolve, reject) => {
+      fflate.unzip(
+        buffer,
+        {
+          filter: (file) => file.name === outputs.originalFilename,
+        },
+        (error, unzipped) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve(unzipped);
         }
-
-        archive.openReadStream(entry, (err, readStream) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          if (!readStream) {
-            // bad @types/yauzl
-            reject(
-              new Error("failed to read archive entry and no reason was given.")
-            );
-            return;
-          }
-
-          const filePath = path.resolve(outdir, outputs.versionedFilename);
-          const fileStream = fs.createWriteStream(filePath);
-
-          readStream.on("error", (error) => reject(error));
-          readStream.on("end", () => {
-            archive.close();
-            fileStream.close((error) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(filePath);
-            });
-          });
-          readStream.pipe(fileStream);
-        });
-      });
-
-      archive.on("end", () => {
-        reject(
-          reason(
-            `expected zip to contain a terraform executable. (${outputs.originalFilename})`
-          )
-        );
-      });
-
-      archive.readEntry();
+      );
     });
-  });
-};
 
-export { extractArchive };
+  if (!fileContent) {
+    throw new Error(
+      `expected zip to contain a terraform executable. (${outputs.originalFilename})`
+    );
+  }
+
+  const filePath = path.resolve(outdir, outputs.versionedFilename);
+  await fs.writeFile(filePath, fileContent);
+  return filePath;
+};
